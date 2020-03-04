f92b095f496b70a822b253b9557a84e4b5dbb26a
Justin Chang <justinyu711@gmail.com>
2020-03-04 22:51:04 +0800

    Change tasks priority and status to integer type

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :find_task, except: [:index, :create, :new]
+  before_action :find_task, except: [:index, :new, :create]
 
   def index 
     @tasks = Task.all
