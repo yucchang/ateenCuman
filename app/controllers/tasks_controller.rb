f4ae4584a6f357c31ad6884d597e43ac1cfbdbf8
Justin Chang <justinyu711@gmail.com>
2020-03-04 18:42:41 +0800

    Feature task CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,54 @@
 class TasksController < ApplicationController
+  before_action :find_task, except: [:index, :create, :new]
+
   def index 
+    @tasks = Task.all
   end 
 
   def show
   end 
 
   def new
+    @task = Task.new 
   end 
 
   def create 
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to root_path, notice: 'Task created.'
+    else 
+      render :new 
+    end
   end 
 
   def edit 
   end 
 
   def update
+    if @task.update(task_params)
+      redirect_to root_path, notice: 'Task updated.'
+    else 
+      render :edit
+    end 
   end 
 
   def destroy
+    @task.delete if @task 
+    redirect_to root_path, notice: 'Task deleted.'
+  end 
+
+  private 
+  def find_task 
+    @task = Task.find(params[:id])
+  end 
+
+  def task_params
+    params.require(:task).permit(:title,
+                                 :content,
+                                 :beginning_at,
+                                 :due_at,
+                                 :priority,
+                                 :status)
   end 
 end 
