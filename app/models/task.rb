31acb5dae02badf44f420f6ff033086982f2a425
Justin Chang <justinyu711@gmail.com>
2020-03-05 18:53:03 +0800

    Apply simple_form

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true
 
-  enum priority: { High: 0, Medium: 1, Low: 2}
+  enum priority: { High: 0, Mid: 1, Low: 2}
   enum status: { Pending: 0, Processing: 1, Done: 2}
 end
