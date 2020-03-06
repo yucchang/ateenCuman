03da7e8558045e9daaf7e47e0fbb7808713b924f
Justin Chang <justinyu711@gmail.com>
2020-03-06 16:11:21 +0800

    Add show template, task filter

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ApplicationRecord
 
   enum priority: { High: 0, Mid: 1, Low: 2}
   enum status: { Pending: 0, Processing: 1, Done: 2}
+
+  scope :incomplete, -> { where(status: [0, 1]) }
 end
