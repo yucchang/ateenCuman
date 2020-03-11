88ec337102365b4319c227f9af6754d5e61a2249
Justin Chang <justinyu711@gmail.com>
2020-03-11 15:12:12 +0800

    Adjust show, index view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,48 +1,48 @@
 class TasksController < ApplicationController
   before_action :find_task, except: [:index, :new, :create]
 
-  def index 
-    @tasks = Task.incomplete
-  end 
+  def index
+    @tasks = Task.order(id: :desc)
+  end
 
   def show
-  end 
+  end
 
   def new
-    @task = Task.new 
-  end 
+    @task = Task.new
+  end
 
-  def create 
+  def create
     @task = Task.new(task_params)
-  
+
 
     if @task.save
       redirect_to root_path, notice: 'Task created.'
-    else 
-      render :new 
+    else
+      render :new
     end
-  end 
+  end
 
-  def edit 
-  end 
+  def edit
+  end
 
   def update
     if @task.update(task_params)
       redirect_to root_path, notice: 'Task updated.'
-    else 
+    else
       render :edit
-    end 
-  end 
+    end
+  end
 
   def destroy
-    @task.delete if @task 
+    @task.delete if @task
     redirect_to root_path, notice: 'Task deleted.'
-  end 
+  end
 
-  private 
-  def find_task 
+  private
+  def find_task
     @task = Task.find(params[:id])
-  end 
+  end
 
   def task_params
     params.require(:task).permit(:title,
@@ -51,5 +51,5 @@ class TasksController < ApplicationController
                                  :due_at,
                                  :priority,
                                  :status)
-  end 
-end 
+  end
+end
